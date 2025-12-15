01a7269676f8344fb9527a078a6b939729ba0469
Archangel <jesse@archangel.wtf>
2025-12-15 16:19:44 +0100

    Remove GUID

--- LotsofLoot/Models/Config/EditablePresetHolder.cs
+++ LotsofLoot/Models/Config/EditablePresetHolder.cs
@@ -4,7 +4,6 @@ namespace LotsofLoot.Models.Config;
 
 public sealed class EditablePresetHolder(LotsofLootPresetConfig config)
 {
-    public Guid Id { get; init; } = Guid.NewGuid();
     public LotsofLootPresetConfig presetConfig { get; init; } = FastCloner.FastCloner.DeepClone(config) ?? throw new InvalidOperationException("Could not clone config!");
     public HashSet<string> PendingChanges { get; init; } = [];
 }
