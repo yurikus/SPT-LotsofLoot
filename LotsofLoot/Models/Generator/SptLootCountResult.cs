0bcd27327f2e834eada68b997a0ed5074f2d6acf
Archangel <jesse@archangel.wtf>
2026-02-19 18:44:44 +0100

    Fix build

---
+++ LotsofLoot/Models/Generator/SptLootCountResult.cs
@@ -0,0 +1,8 @@
+﻿namespace LotsofLoot.Models.Generator;
+
+public sealed record SptLootCountResult
+{
+    public int StaticContainersGenerated { get; set; } = 0;
+    public int StaticItemsSpawned { get; set; } = 0;
+    public int DynamicItemsSpawned { get; set; } = 0;
+}
